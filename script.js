9c41e8736ca07cbde6a0ec52ebfe55ced04156bd
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-10-04 20:53:13 +0300

    update HTML-CSS

--- script.js
+++ script.js
@@ -29,9 +29,10 @@ checkWinner()
     // add logic to keep track of where X and O is
 
 }
-
+// Grab the class title to show who wins the Match
 function checkWinner() {
     let result = document.querySelector('.title')
+//  give 8 winning possibilities to X 
     if (item1.innerHTML === 'X' && item2.innerHTML === 'X' && item3.innerHTML === 'X'){
         result.innerText = 'Sub-Zero wins' 
      document.querySelector(".title").style.color="rgb(0, 187, 255)" 
@@ -65,7 +66,7 @@ function checkWinner() {
         document.querySelector(".title").style.color="rgb(0, 187, 255)" 
     }
 
-
+// give 8 winning possibilities to O
     else if (item1.innerHTML === 'O' && item2.innerHTML === 'O' && item3.innerHTML === 'O'){
         result.innerText = 'Scorpion wins' 
      document.querySelector(".title").style.color="rgb(255, 68, 0)" 
@@ -98,6 +99,7 @@ function checkWinner() {
         result.innerText = 'Scorpion wins' 
         document.querySelector(".title").style.color="rgb(255, 68, 0)" 
     }
+    // if all the blocks are filled without matching the above 16 possibilities, write/insert "Draw"
     else if (item1.innerHTML !=='' && item2.innerHTML !=='' && item3.innerHTML !=='' &&
     item4.innerHTML !=='' && item5.innerHTML !=='' && item6.innerHTML !=='' && item7.innerHTML !==''
     && item8.innerHTML !=='' && item9.innerHTML !=='') {
