c71b978e78c9a529f7c4af7e297499ab64e2f4e3
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-10-05 21:31:47 +0300

    Read me file updated

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # project-1-Tic-Tac-Toe
+-------------------------------------------------------------------------------------
+
+a description of the app
+
+this is an X and O Game
+
+-------------------------------------------------------------------------------------
+
+any sources used :
+the below website was used as a reference for the reset button's (Fight) CSS styling  
+https://codepen.io/KittyGiraudel/pen/Dpobgy
+
+-------------------------------------------------------------------------------------
+
+the approach taken :
+
+
+-------------------------------------------------------------------------------------
+
+explanations of the technologies used :
+
+i've used HTML, CSS and JavaScript to build this game
+
+-------------------------------------------------------------------------------------
+
+Live link of the game :
+
+
+-------------------------------------------------------------------------------------
+
+a post-project reflection with any unsolved problems or growth areas you may have had
+
+
+-------------------------------------------------------------------------------------
+
+please find the below instructions on how to play the game
+
+instructions:
+* the game should be played by 2 players only.
+* the player one is called (Sub-Zero) plays with (X) 
+* the player two is called (Scorpion) plays with (O)
+* the first challenge will begin by (Sub-Zero X)
+* the game will be played within the area of the 9 blocks
+* The first player to get 3 of his marks in a row (up, down, across, or diagonally) is the winner.
+* when all the blocks are filled without a winner, it will be considered as "Draw"
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+
 // add a function and make the divs (grid) clickable, depending on the players turn
 // link the "play" function to the 9 Id's that we've created in the HTML
 
-// let turn = 'Player 1';
+// Get all the blocks and the titles from the HTML, define the players turn
+//Define the reset button, and add an event listener
 
 let blocks = document.querySelectorAll('.block')
 
 let playerOneTurn = true
-
-let empty = ["","","","","","","","",""]
 
 let turnDisplay = document.querySelector('.title')
 
@@ -15,13 +15,22 @@ let reset = document.getElementById('resetButton')
 
 reset.addEventListener('click', resetGame)
 
+
+// add and define the audios
+
 let scorpionSound = new Audio('./scorpion-wins.mp3')
 let subzeroSound = new Audio('./Sub-Zero wins.mp3')
 let fightSound = new Audio('./Mortal Kombat.mp3')
 
+
+//add an event listener with a click event to make the blocks clickable
+
 blocks.forEach(div => {
     div.addEventListener('click', play)
 })
+
+
+// add a loop function that gives input to both players, and separate their turns.
 
 function play(event) {
     let fill = document.getElementById(event.target.id);
@@ -46,6 +55,8 @@ function play(event) {
     checkWinner()
 }
 
+// add a function to deactivate the click event
+
 function deActivate(event) {
     event.target.removeEventListener('click', play);
 }
@@ -56,7 +67,10 @@ function checkWinner() {
     let result = document.querySelector('.title')
     
     
-    //  give 8 winning possibilities to X 
+    //  give 8 winning possibilities to X, and add the winner's name in the title with a blue color for Sub-zero and red color for scorpion.
+    // remove the event listener to stop the game whenever a player wins.
+    // stop the background Audio and start another audio to annouce the winner's name.
+
     if (item1.innerHTML === 'X' && item2.innerHTML === 'X' && item3.innerHTML === 'X'){
         result.innerText = 'Sub-Zero Wins' 
         document.querySelector(".title").style.color="rgb(0, 187, 255)" 
@@ -172,13 +186,20 @@ function checkWinner() {
         scorpionSound.play();
         fightSound.pause();
     }
-    // if all the blocks are filled without matching the above 16 possibilities, write/insert "Draw"
+
+
+    // if all the blocks are filled without matching one of the above 16 possibilities, write/insert "Draw"
+
     else if (item1.innerHTML !=='' && item2.innerHTML !=='' && item3.innerHTML !=='' &&
     item4.innerHTML !=='' && item5.innerHTML !=='' && item6.innerHTML !=='' && item7.innerHTML !==''
     && item8.innerHTML !=='' && item9.innerHTML !=='') {
         result.innerText = 'Draw'
 }
 }
+
+
+// write a function to reset and clear the blocks input to start a new game and replay the background audio
+
 function resetGame(){
     blocks.forEach(block => {
         block.innerText = ''
@@ -188,7 +209,6 @@ function resetGame(){
         div.addEventListener('click', play)
         fightSound.play();
         fightSound.currentTime=0;
-        
     })
     playerOneTurn = true
  }
